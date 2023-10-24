ce199a6df2bf97546fdbb720fc6ca42cf863ea30
Hrithik Sagar <hrithiksagar36@gmail.com>
2023-10-24 12:59:40 +0530

    update fetch data

--- opening a CSV file frm url.py
+++ opening a CSV file frm url.py
@@ -2,7 +2,6 @@ import requests
 from io import StringIO
 import pandas as pd
 
-
 url = "https://raw.githubusercontent.com/cs109/2014_data/master/countries.csv"
 headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
 req = requests.get(url, headers=headers)
